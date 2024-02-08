ab4450597a289a2d9b09d6fb3acbdaaef88153e9
nolanggz <87678275+eclipse606@users.noreply.github.com>
2024-02-08 02:20:53 -0300

    little fix to reset_system.lua
    remove bool use_back_level 
    
    (what reset system which no use back level bruh?)
    now all working done

--- data/lib/reset_system.lua
+++ data/lib/reset_system.lua
@@ -1,15 +1,14 @@
 --[[
 
--- CASO SAIBA USAR O MODO SIMPLIFICADO, USE ABAIXO, SE NÃO, USE A TABELA PADRÃO
+-- CASO SAIBA USAR O MODO SIMPLIFICADO, USE ABAIXO, SE NÃƒO, USE A TABELA PADRÃƒO
 
 ResetSystem = {
 	back_to_level = 8, -- level que vai voltar
-	use_back_level = true,
 	resets = {},
-	incrementToReset = 250, -- qual a diferença de level a cada rr?
+	incrementToReset = 250, -- qual a diferenÃ§a de level a cada rr?
 	firstResetAt = 1500,	-- qual o level do PRIMEIRO rr
 	
-	-- incrementToReset se for 250 e firstResetAt = 1000, os resets serão 1000/1250/1500/1750
+	-- incrementToReset se for 250 e firstResetAt = 1000, os resets serÃ£o 1000/1250/1500/1750
 }
 
 for i = 0, 100 do
@@ -21,11 +20,9 @@ for i = 0, 100 do
 	ResetSystem.resets[i] = {needed_level = level, exp_percent = exp_percent, damage_percent = damage_percent, hpmp_percent = hpmp_percent}
 end
 ]]--
-
-ResetSystem = {
-	back_to_level = 8, -- level que vai voltar
-	use_back_level = true,
 	
+ResetSystem = {
+	back_to_level = 50, -- level que vai voltar
 	resets = {
 		[1] =  {needed_level = 1000,  exp_percent = 2,   damage_percent = 2,   hpmp_percent = 10},
 		[2] =  {needed_level = 2000,  exp_percent = 4,   damage_percent = 4,   hpmp_percent = 20},
@@ -36,11 +33,10 @@ ResetSystem = {
 		[7] =  {needed_level = 7000,  exp_percent = 14,  damage_percent = 14,  hpmp_percent = 70},
 		[8] =  {needed_level = 8000,  exp_percent = 16,  damage_percent = 16,  hpmp_percent = 80},
 		[9] =  {needed_level = 9000,  exp_percent = 18,  damage_percent = 18,  hpmp_percent = 90},
-		[10] = {needed_level = 10000, exp_percent = 20,  damage_percent = 20,  hpmp_percent = 100},
-		-- ....
+		[10] = {needed_level = 10000, exp_percent = 20,  damage_percent = 20,  hpmp_percent = 100}
+		-- etc...
 	}
 }
-
 
 function ResetSystem:getCount(pid)
 	return getPlayerResets(pid)
@@ -76,8 +72,8 @@ function ResetSystem:updateHealthAndMana(pid)
 			local oldMaxMana = getCreatureMaxMana(pid)
 			
 			local level = getPlayerLevel(pid)
-			local totalHealth = (185 - vocationInfo.healthGain * 8) + (vocationInfo.healthGain * level)
-			local totalMana = (185 - vocationInfo.manaGain * 8) + (vocationInfo.manaGain * level)
+			local totalHealth = (vocationInfo.healthGain * level)
+			local totalMana = (vocationInfo.manaGain * level)
 			
 			local newMaxHealth = math.floor(totalHealth + (totalHealth * (bonus.hpmp_percent / 100)))
 			local newMaxMana = math.floor(totalMana + (totalMana * (bonus.hpmp_percent / 100)))
@@ -99,19 +95,19 @@ function ResetSystem:updateHealthAndMana(pid)
 	end
 end
 
-
 function ResetSystem:execute(pid)
 	local playerLevel = getPlayerLevel(pid)
-	if (playerLevel > self.back_to_level and use_back_level) then
+	if (playerLevel > self.back_to_level) then
 		doPlayerAddExperience(pid, getExperienceForLevel(self.back_to_level) - getPlayerExperience(pid))
 		playerLevel = self.back_to_level
 	end
+
 	self:addCount(pid)
 	self:applyBonuses(pid)
 	self:updateHealthAndMana(pid)
 	local bonus = self:getInfo(pid)
 	if (bonus) then
-		local message = "Você efetuou seu " .. self:getCount(pid) .. "° reset."
+		local message = "VocÃª efetuou seu " .. self:getCount(pid) .. "Â° reset."
 		if (bonus.damage_percent) then
 			message = message .. "\n+" .. bonus.damage_percent .. "% de dano"
 		end
